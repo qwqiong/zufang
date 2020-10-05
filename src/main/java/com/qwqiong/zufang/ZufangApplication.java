26a273bbcd84ec03f6ff915bfc427771448aab71
qwqiong <mossader@qq.com>
2020-10-05 17:05:06 +0800

    项目初始化

---
+++ src/main/java/com/qwqiong/zufang/ZufangApplication.java
@@ -0,0 +1,13 @@
+package com.qwqiong.zufang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZufangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZufangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qwqiong/zufang/ZufangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwqiong.zufang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZufangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
